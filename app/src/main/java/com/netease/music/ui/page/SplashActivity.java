21d95a46953fd3e6e7efd992d654515d851848b9
zion223 <1025618933@qq.com>
2020-08-15 16:09:14 +0800

    feat: 添加闪屏页

--- app/src/main/java/com/netease/music/ui/page/SplashActivity.java
+++ app/src/main/java/com/netease/music/ui/page/SplashActivity.java
@@ -3,13 +3,12 @@ package com.netease.music.ui.page;
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.text.TextUtils;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.kunminx.architecture.ui.page.BaseActivity;
-import com.kunminx.architecture.ui.page.DataBindingConfig;
+import com.imooc.lib_common_ui.utils.SharePreferenceUtil;
 import com.kunminx.architecture.utils.Utils;
 import com.netease.music.R;
 
@@ -21,61 +20,51 @@ import io.reactivex.disposables.Disposable;
 import pub.devrel.easypermissions.EasyPermissions;
 
 
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = "SplashActivity";
 
 
     @Override
-    protected void initViewModel() {
-
-    }
-
-    @Override
-    protected DataBindingConfig getDataBindingConfig() {
-        return new DataBindingConfig(R.layout.delegate_splash,0,null);
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        //ScreenUtils.setStatusBarColor(this, Color.parseColor("#Db2C1F"));
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.delegate_splash);
         String[] perms = {Manifest.permission.READ_PHONE_STATE
                 , Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
                 , Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
         if (!EasyPermissions.hasPermissions(this, perms)) {
             EasyPermissions.requestPermissions(this, "请打开相关权限", 1, perms);
-        }else{
-            Observable.timer(2, TimeUnit.SECONDS)
-                    .subscribe(new Observer<Long>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-
-                        }
-
-                        @Override
-                        public void onNext(Long aLong) {
-                            String authToken = SharePreferenceUtil.getInstance(Utils.getApp()).getAuthToken("");
-                            if (TextUtils.isEmpty(authToken)) {
-                                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-                            } else {
-                                //startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-                            }
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-
-                        }
-
-                        @Override
-                        public void onComplete() {
-
-                        }
-                    });
         }
+        Observable.timer(2, TimeUnit.SECONDS)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
 
+                    }
 
+                    @Override
+                    public void onNext(Long aLong) {
+                        String authToken = SharePreferenceUtil.getInstance(Utils.getApp()).getAuthToken("");
+                        if (TextUtils.isEmpty(authToken)) {
+                            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                        } else {
+                            //进入主界面
+                            //startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
-
 }
+
