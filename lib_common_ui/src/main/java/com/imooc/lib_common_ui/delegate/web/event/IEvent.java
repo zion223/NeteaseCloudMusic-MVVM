af3bdd94d662de0e885fea20c897eda757df80f1
zion223 <1025618933@qq.com>
2020-08-15 16:00:51 +0800

    feat: 去除web界面组件

--- lib_common_ui/src/main/java/com/imooc/lib_common_ui/delegate/web/event/IEvent.java
+++
@@ -1,6 +0,0 @@
-package com.imooc.lib_common_ui.delegate.web.event;
-
-public interface IEvent {
-
-	String execute(String params);
-}
--- lib_common_ui/src/main/java/com/imooc/lib_common_ui/delegate/web/route/RouteKeys.java
+++
@@ -1,5 +0,0 @@
-package com.imooc.lib_common_ui.delegate.web.route;
-
-public enum RouteKeys {
-	URL
-}
