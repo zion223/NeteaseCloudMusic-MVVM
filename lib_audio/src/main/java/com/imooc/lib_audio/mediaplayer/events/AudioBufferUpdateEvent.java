854e5bba884e0ec22b11b67dd615c8511d8a29bd
zion223 <1025618933@qq.com>
2020-08-17 14:49:52 +0800

    feat: 添加lib_audio模块

---
+++ lib_audio/src/main/java/com/imooc/lib_audio/mediaplayer/events/AudioBufferUpdateEvent.java
@@ -0,0 +1,9 @@
+package com.imooc.lib_audio.mediaplayer.events;
+
+public class AudioBufferUpdateEvent {
+    public final int progress;
+
+    public AudioBufferUpdateEvent(int progress) {
+        this.progress = progress;
+    }
+}
