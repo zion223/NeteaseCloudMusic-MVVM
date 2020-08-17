87cc32bdcce99b4252ce39a98eb587bea268a9b3
zion223 <1025618933@qq.com>
2020-08-17 12:26:15 +0800

    feat: 更新BaseRecyclerViewAdapterHelper版本到3.0.4 (Androidx)

--- lib_api/src/main/java/com/imooc/lib_api/model/dj/DjToplistEntity.java
+++ lib_api/src/main/java/com/imooc/lib_api/model/dj/DjToplistEntity.java
@@ -1,15 +1,17 @@
 package com.imooc.lib_api.model.dj;
 
 
+import com.chad.library.adapter.base.entity.JSectionEntity;
 import com.chad.library.adapter.base.entity.SectionEntity;
 
-public class DjToplistEntity extends SectionEntity<DjRankListBean.List> {
+class DjToplistEntitity {
+//public class DjToplistEntity extends SectionEntity<DjRankListBean.List> {
 
-    public DjToplistEntity(DjRankListBean.List list) {
-        super(list);
-    }
-
-    public DjToplistEntity(boolean isHeader, String header) {
-        super(isHeader, header);
-    }
+//    public DjToplistEntity(DjRankListBean.List list) {
+//        super(list);
+//    }
+//
+//    public DjToplistEntity(boolean isHeader, String header) {
+//        super(isHeader, header);
+//    }
 }
--- lib_api/src/main/java/com/imooc/lib_api/model/dj/RadioSortEntity.java
+++ lib_api/src/main/java/com/imooc/lib_api/model/dj/RadioSortEntity.java
@@ -3,13 +3,14 @@ package com.imooc.lib_api.model.dj;
 import com.chad.library.adapter.base.entity.SectionEntity;
 import com.imooc.lib_api.model.dj.DjCatelistBean;
 
-public class RadioSortEntity extends SectionEntity<DjCatelistBean.CategoriesBean> {
+public class RadioSortEntity {
+//public class RadioSortEntity extends SectionEntity<DjCatelistBean.CategoriesBean> {
 
-    public RadioSortEntity(boolean isHeader, String header) {
-        super(isHeader, header);
-    }
-
-    public RadioSortEntity(DjCatelistBean.CategoriesBean bean) {
-        super(bean);
-    }
+//    public RadioSortEntity(boolean isHeader, String header) {
+//        super(isHeader, header);
+//    }
+//
+//    public RadioSortEntity(DjCatelistBean.CategoriesBean bean) {
+//        super(bean);
+//    }
 }
