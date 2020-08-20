2f7e96050663b53c95b44d46977c75f6116ad8b7
zion223 <1025618933@qq.com>
2020-08-20 14:39:05 +0800

    feat: 主页设置为发现页,去除多余的变量,设置offsetLimit为1

--- app/src/main/java/com/netease/music/ui/state/MainViewModel.java
+++ app/src/main/java/com/netease/music/ui/state/MainViewModel.java
@@ -3,40 +3,26 @@ package com.netease.music.ui.state;
 import androidx.databinding.ObservableBoolean;
 import androidx.databinding.ObservableField;
 import androidx.databinding.ObservableInt;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.netease.music.data.bean.TestAlbum;
 import com.netease.music.data.config.CHANNEL;
-import com.netease.music.domain.request.MusicRequest;
-
-import java.util.List;
 
 public class MainViewModel extends ViewModel {
 
     public final ObservableBoolean initTabAndPage = new ObservableBoolean();
 
-    public final ObservableField<String> pageAssetPath = new ObservableField<>();
 
     //头部数据
     public final ObservableField<CHANNEL[]> channelArray = new ObservableField<>();
     //offsetLimt
     public final ObservableInt limit = new ObservableInt();
-
-
-    public final MutableLiveData<List<TestAlbum.TestMusic>> list = new MutableLiveData<>();
-
-    public final MutableLiveData<Boolean> notifyCurrentListChanged = new MutableLiveData<>();
-
-    //TODO 将 request 作为 ViewModel 的成员暴露给 Activity/Fragment，
-    // 如此便于语义的明确，以及实现多个 request 在 ViewModel 中的组合和复用。
-
-    public final MusicRequest musicRequest = new MusicRequest();
+    //当前页为发现页
+    public final ObservableInt currentItem = new ObservableInt();
 
     {
-        limit.set(4);
+        limit.set(1);
+        currentItem.set(1);
         initTabAndPage.set(true);
-        pageAssetPath.set("summary.html");
         channelArray.set(new CHANNEL[]{CHANNEL.MY, CHANNEL.DISCORY, CHANNEL.YUNCUN, CHANNEL.VIDEO});
     }
 
