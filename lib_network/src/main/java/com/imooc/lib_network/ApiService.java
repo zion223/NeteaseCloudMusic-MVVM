004bf3628a8ebd9b6346bb2afe888251ea9fd79e
zion223 <1025618933@qq.com>
2020-08-25 11:48:56 +0800

    docs: 添加评论接口的注释,修改TYPE对应

--- lib_network/src/main/java/com/imooc/lib_network/ApiService.java
+++ lib_network/src/main/java/com/imooc/lib_network/ApiService.java
@@ -228,6 +228,14 @@ public interface ApiService {
     @GET("related/allvideo")
     Observable<VideoRelatedBean> getVideoRelated(@Query("id") String id);
 
+    /**
+     * 给评论点赞
+     * id : 资源 id, 如歌曲 id, mv id
+     * cid : 评论 id
+     * t : 是否点赞 ,1 为点赞 ,0 为取消点赞
+     * tpye: 数字 , 资源类型 , 对应歌曲 , mv, 专辑 , 歌单 , 电台, 视频对应以下类型
+     * 0: 歌曲 1: mv 2: 歌单 3: 专辑 4: 电台 5: 视频 6: 动态
+     */
     @GET("comment/like")
     Observable<CommentLikeBean> likeComment(@Query("id") long id, @Query("cid") long cid, @Query("t") int t, @Query("type") int type);
 
