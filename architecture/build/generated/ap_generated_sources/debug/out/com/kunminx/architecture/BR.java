92ca93aa327ff09c0a82eed3823764ee6e84b00c
zion223 <1025618933@qq.com>
2020-08-28 13:20:00 +0800

    docs: 更新build.grale文件

---
+++ architecture/build/generated/ap_generated_sources/debug/out/com/kunminx/architecture/BR.java
@@ -0,0 +1,5 @@
+package com.kunminx.architecture;
+
+public class BR {
+  public static int _all = 0;
+}
---
+++ architecture/build/generated/ap_generated_sources/debug/out/com/kunminx/strictdatabinding/BR.java
@@ -0,0 +1,5 @@
+package com.kunminx.strictdatabinding;
+
+public class BR {
+  public static int _all = 0;
+}
