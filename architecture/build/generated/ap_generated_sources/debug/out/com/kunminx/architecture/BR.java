7c332ed26dcfd4f1f77328112e3ec669a4d91f15
zion223 <1025618933@qq.com>
2020-08-30 20:14:06 +0800

    refactor: 删除多余的文件

--- architecture/build/generated/ap_generated_sources/debug/out/com/kunminx/architecture/BR.java
+++
@@ -1,5 +0,0 @@
-package com.kunminx.architecture;
-
-public class BR {
-  public static int _all = 0;
-}
--- architecture/build/generated/ap_generated_sources/debug/out/com/kunminx/strictdatabinding/BR.java
+++
@@ -1,5 +0,0 @@
-package com.kunminx.strictdatabinding;
-
-public class BR {
-  public static int _all = 0;
-}
