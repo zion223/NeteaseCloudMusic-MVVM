c0c7d72144462d77161dfb9dda3f98da2c441624
zion223 <1025618933@qq.com>
2020-09-23 17:13:32 +0800

    refactor: 规范代码顺序

--- app/src/main/java/com/netease/music/data/config/TYPE.java
+++ app/src/main/java/com/netease/music/data/config/TYPE.java
@@ -3,22 +3,22 @@ package com.netease.music.data.config;
 public enum TYPE {
 
 
-    PLAYLIST("歌单", 0x02),
+    SONG("歌曲", 0x00),
     MV("MV", 0x01),
+    PLAYLIST("歌单", 0x02),
     ALBUM("专辑", 0x03),
     RADIO("电台", 0x04),
     VIDEO("视频", 0x05),
-    EVENT("动态", 0x06),
-    SONG("歌曲", 0x00);
+    EVENT("动态", 0x06);
 
     //所有类型标识
-    public static final int PLAYLIST_ID = 0x02;
+    public static final int SONG_ID = 0x00;
     public static final int MV_ID = 0x01;
+    public static final int PLAYLIST_ID = 0x02;
     public static final int ALBUM_ID = 0x03;
     public static final int RADIO_ID = 0x04;
     public static final int VIDEO_ID = 0x05;
     public static final int EVENT_ID = 0x06;
-    public static final int SONG_ID = 0x00;
 
 
     private final String key;
