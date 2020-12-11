6bf8d8fc43b8883c3939229b39e782d2322419cb
zion223 <1025618933@qq.com>
2020-12-11 10:05:04 +0800

    feat: 侧边栏开启新Activity时先关闭自己

--- app/src/main/java/com/netease/music/ui/page/drawer/DrawerFragment.java
+++ app/src/main/java/com/netease/music/ui/page/drawer/DrawerFragment.java
@@ -4,15 +4,18 @@ import com.kunminx.architecture.ui.page.BaseFragment;
 import com.kunminx.architecture.ui.page.DataBindingConfig;
 import com.netease.music.R;
 import com.netease.music.BR;
+import com.netease.music.ui.callback.SharedViewModel;
 import com.netease.music.ui.state.DrawerViewModel;
 
 public class DrawerFragment extends BaseFragment {
 
     private DrawerViewModel mDrawerViewModel;
+    private SharedViewModel mSharedViewModel;
 
     @Override
     protected void initViewModel() {
         mDrawerViewModel = getFragmentScopeViewModel(DrawerViewModel.class);
+        mSharedViewModel = getApplicationScopeViewModel(SharedViewModel.class);
     }
 
     @Override
@@ -26,16 +29,20 @@ public class DrawerFragment extends BaseFragment {
 
         //查看消息
         public void searchMessage() {
+            // 关闭侧边栏
+            mSharedViewModel.requestToOpenOrCloseDrawer(false);
             nav().navigate(R.id.action_drawerFragment_to_messageTabActivity);
         }
 
         //查看音乐云盘
         public void searchCloud() {
+            mSharedViewModel.requestToOpenOrCloseDrawer(false);
             nav().navigate(R.id.action_drawerFragment_to_cloudActivity);
         }
 
         //登录界面
         public void login() {
+            mSharedViewModel.requestToOpenOrCloseDrawer(false);
             nav().navigate(R.id.action_drawerFragment_to_loginActivity);
         }
     }
