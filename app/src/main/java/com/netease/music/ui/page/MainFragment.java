b9323c54b1e1dbf3804164eb6c7f01c8d301e283
zion223 <1025618933@qq.com>
2020-12-28 10:03:49 +0800

    refactor: 删除多余的import

--- app/src/main/java/com/netease/music/ui/page/MainFragment.java
+++ app/src/main/java/com/netease/music/ui/page/MainFragment.java
@@ -2,9 +2,7 @@ package com.netease.music.ui.page;
 
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
