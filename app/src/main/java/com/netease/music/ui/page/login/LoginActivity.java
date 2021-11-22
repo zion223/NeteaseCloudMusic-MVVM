82495327e1d321f409fa9e300ca7406eaed07fdc
zion223 <1025618933@qq.com>
2021-11-22 13:14:33 +0800

    refactor: 优化登录界面效果 feat:登录时若没有网络给予提示

--- app/src/main/java/com/netease/music/ui/page/login/LoginActivity.java
+++ app/src/main/java/com/netease/music/ui/page/login/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.netease.music.ui.page.login;
 
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.kunminx.architecture.ui.page.BaseActivity;
 import com.kunminx.architecture.ui.page.DataBindingConfig;
+import com.kunminx.architecture.utils.NetworkUtils;
 import com.netease.music.BR;
 import com.netease.music.R;
 import com.netease.music.ui.state.LoginViewModel;
@@ -28,8 +30,11 @@ public class LoginActivity extends BaseActivity {
 
         public void phoneLogin() {
             if (mLoginViewModel.argeeRule.get()) {
+                if (!NetworkUtils.isConnected()) {
+                    Toast.makeText(getBaseContext(), getBaseContext().getString(R.string.check_network), Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 startActivity(new Intent(LoginActivity.this, PhoneLoginActivity.class));
-                finish();
             } else {
                 //提示用户点击同意条款
                 showShortToast("请点击同意《用户协议》和《隐私政策》《儿童隐私政策》");
