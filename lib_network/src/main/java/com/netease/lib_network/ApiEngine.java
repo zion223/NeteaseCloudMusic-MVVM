1a14dbb195578a782e18408b08bfdedf56b4f6d2
zion223 <1025618933@qq.com>
2021-11-23 15:45:26 +0800

    refactor: 优化代码

--- lib_network/src/main/java/com/netease/lib_network/ApiEngine.java
+++ lib_network/src/main/java/com/netease/lib_network/ApiEngine.java
@@ -4,7 +4,6 @@ import com.franmontiel.persistentcookiejar.ClearableCookieJar;
 import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
 import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
-import com.google.gson.Gson;
 import com.kunminx.architecture.utils.Utils;
 import com.netease.lib_network.interceptor.NetWorkInterceptor;
 import com.netease.lib_network.interceptor.ResponseInterceptor;
@@ -51,11 +50,10 @@ public class ApiEngine {
 //                .cache(cache)
                 .build();
 
-        Gson gson = new Gson();
         retrofit = new Retrofit.Builder()
                 .baseUrl(ApiService.BASE_URL)
                 .client(client)
-                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
     }
